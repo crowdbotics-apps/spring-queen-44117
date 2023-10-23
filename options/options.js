fea0854a181996786b4cd81488f7762335669d9c
Crowdbotics <team@crowdbotics.com>
2023-10-23 17:39:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-queen-44117",
+  url: "https://spring-queen-44117.botics.co",
+  api: "https://spring-queen-44117.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
